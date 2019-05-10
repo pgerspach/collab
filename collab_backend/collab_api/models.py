8283ebe2c6ce9e336c6c509d81d960018bf7c02b
pgerspach <pgerspac@alumni.nd.edu>
2019-05-09 21:14:03 -0400

    many to one between songs and users

--- collab_backend/collab_api/models.py
+++ collab_backend/collab_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Song(models.Model):
@@ -8,5 +9,5 @@ class Song(models.Model):
         song.save()
         return song
 
-    user = models.CharField(max_length=30)
-    name = models.CharField(max_length=30, default='song')
+    name = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
